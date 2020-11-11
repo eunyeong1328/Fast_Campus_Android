4cc5625d85e81b35d097cb7058edae35f3106467
JayeHa <jwha94@gmail.com>
2020-11-11 15:23:03 +0900

    왜 회원가입창 가는거 안돼...

--- Shop/src/main/java/com/project/shop/view/member/MemberController.java
+++ Shop/src/main/java/com/project/shop/view/member/MemberController.java
@@ -4,16 +4,17 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.project.shop.member.MemberService;
 import com.project.shop.member.MemberVO;
 
 @Controller("memberController")
@@ -52,5 +53,34 @@ public class MemberController {
 //		}
 		mav.setViewName("/member/loginForm");
 		return mav;
+	}
+	
+
+	@RequestMapping(value="/addMember.do" ,method = RequestMethod.POST)
+	public ResponseEntity addMember(@ModelAttribute("memberVO") MemberVO _memberVO,
+			                HttpServletRequest request, HttpServletResponse response) throws Exception {
+		response.setContentType("text/html; charset=UTF-8");
+		request.setCharacterEncoding("utf-8");
+		String message = null;
+		ResponseEntity resEntity = null;
+		HttpHeaders responseHeaders = new HttpHeaders();
+		responseHeaders.add("Content-Type", "text/html; charset=utf-8");
+		/*try {
+		    memberService.addMember(_memberVO);
+		    message  = "<script>";
+		    message +=" alert('회원 가입을 마쳤습니다.로그인창으로 이동합니다.');";
+		    message += " location.href='"+request.getContextPath()+"/member/loginForm.do';";
+		    message += " </script>";
+		    
+		}catch(Exception e) {
+			message  = "<script>";
+		    message +=" alert('작업 중 오류가 발생했습니다. 다시 시도해 주세요');";
+		    message += " location.href='"+request.getContextPath()+"/member/memberForm.do';";
+		    message += " </script>";
+			e.printStackTrace();
+		}*/
+		message += " location.href='"+request.getContextPath()+"/member/loginForm.do';";
+		resEntity =new ResponseEntity(message, responseHeaders, HttpStatus.OK);
+		return resEntity;
 	}
 }
