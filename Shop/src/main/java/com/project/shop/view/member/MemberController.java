9b5f127b491105b332ea6f42e2810a6897d84b1e
JayeHa <jwha94@gmail.com>
2020-11-13 15:42:12 +0900

    쓸데없는 주석 지움

--- Shop/src/main/java/com/project/shop/view/member/MemberController.java
+++ Shop/src/main/java/com/project/shop/view/member/MemberController.java
@@ -1,8 +1,5 @@
 package com.project.shop.view.member;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -11,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.project.shop.common.base.BaseController;
@@ -57,42 +53,6 @@ public class MemberController extends BaseController{
 		return mav;
 	}
 
-//	@Override
-//	@RequestMapping(value="/addMember.do" ,method = RequestMethod.POST)
-//	public ResponseEntity addMember(@ModelAttribute("memberVO") MemberVO _memberVO,
-//			                HttpServletRequest request, HttpServletResponse response) throws Exception {
-//		response.setContentType("text/html; charset=UTF-8");
-//		request.setCharacterEncoding("utf-8");
-//		String message = null;
-//		ResponseEntity resEntity = null;
-//		HttpHeaders responseHeaders = new HttpHeaders();
-//		responseHeaders.add("Content-Type", "text/html; charset=utf-8");
-//		try {
-//		    memberService.addMember(_memberVO);
-//		    message  = "<script>";
-//		    message +=" alert('ȸ�� ������ ���ƽ��ϴ�.�α���â���� �̵��մϴ�.');";
-//		    message += " location.href='"+request.getContextPath()+"/member/loginForm.do';";
-//		    message += " </script>";
-//		    
-//		}catch(Exception e) {
-//			message  = "<script>";
-//		    message +=" alert('�۾� �� ������ �߻��߽��ϴ�. �ٽ� �õ��� �ּ���');";
-//		    message += " location.href='"+request.getContextPath()+"/member/memberForm.do';";
-//		    message += " </script>";
-//			e.printStackTrace();
-//		}
-//		resEntity =new ResponseEntity(message, responseHeaders, HttpStatus.OK);
-//		return resEntity;
-//	}
-//	
-//	@Override
-//	@RequestMapping(value="/overlapped.do" ,method = RequestMethod.POST)//id �ߺ��˻�
-//	public ResponseEntity overlapped(@RequestParam("id") String id,HttpServletRequest request, HttpServletResponse response) throws Exception{
-//		ResponseEntity resEntity = null;
-//		String result = memberService.overlapped(id);
-//		resEntity =new ResponseEntity(result, HttpStatus.OK);
-//		return resEntity;
-//	}
 
 	
 }
